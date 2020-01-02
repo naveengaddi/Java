b79a0d0cce07ea4d3076eab237f552dec4f5d580
naveengaddi <46519632+naveengaddi@users.noreply.github.com>
2020-01-02 16:23:20 +0530

    Update TicTacToeTest.java

--- Task/TicTacToe/TicTacToeTest.java
+++ Task/TicTacToe/TicTacToeTest.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 
 public class TicTacToeTest{
 	public static void main(String[] args) {
-		Player p1 = new Player("Naveen",'X');
-		Player p2 = new Player("Gaddi",'O');
+		// Player p1 = new Player("Naveen",'X');
+		// Player p2 = new Player("Gaddi",'O');
 
-		TicTacToeGame tGame = new TicTacToeGame();
-		tGame.startGame(p1,p2);
+		TicTacToeGame tGame = new TicTacToeGame("Naveen","Mani");
+		tGame.startGame();
 	}
 }
